4e7c0f1c838310e5fae12e22466fc128bd159470
LayMui <laymui.toh@taiger.com>
2020-12-29 21:13:10 +0800

    Validation on AddJob

--- src/views/AddJob.vue
+++ src/views/AddJob.vue
@@ -26,31 +26,37 @@
           <label class="block uppercase tracking-wide text-gray-800 text-xs font-bold mb-2">Date</label>
           <input type="date" name="name" data-qa="jobDate" v-model="job.date" class="appearance-none block w-full bg-white text-gray-800 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-grey"/>
         </div>
-
-        <button :disabled="$v.$invalid" @click="submitJob" data-qa="jobSubmit" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded text-xs">
+       
+          <button :disabled="!job.name" @click="submitJob" data-qa="jobSubmit" class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded text-xs">
           Submit
         </button>
-      </div>
+        </div>
+      
 </template>
 
 <script>
 // @ is an alias to /src
 
 import LivePreview from '@/components/LivePreview'
-import { jobsData } from "@/data/job"
 import { required, minLength } from 'vuelidate/lib/validators'
 export default {
   name: 'AddJob', //for debugging purpose
   data() {
       return {
-        job: jobsData[0],
-        submitStatus: null
+        job: {
+          name: "",
+          duration: "",
+          date: "",
+        
+        }
       }
   },
   validations: {
-    name: {
-      required,
-      minLength: minLength(4)
+    job: {
+      name: {
+        required,
+        minLength: minLength(4)
+      }
     }
   },
   components: {
@@ -58,14 +64,7 @@ export default {
   },
   methods: {
     submitJob() {
-        this.$v.$touch()
-        if (this.$v.$invalid) {
-          this.submitStatus = 'ERROR'
-        } else {
-          this.submitStatus = 'PENDING'
-          setTimeout(() => {
-            this.submitStatus = 'OK'}, 500)
-        }
+       
         // 1. Get all the job from local storage
         // 2. Add the new job inside it
         // 3. Store it back inside the local storage
